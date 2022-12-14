a89be7454efba97a97e53c621285a0a21acd5f15
Dead_Moroz <malussapiens@gmail.com>
2022-12-14 22:21:09 +0300

    modified

--- Seminar1/S1T5/s1t5.py
+++ Seminar1/S1T5/s1t5.py
@@ -26,4 +26,5 @@ if validate_input(x1, y1) and validate_input(x2, y2):
     x1, y1 = float(x1), float(y1)
     x2, y2 = float(x2), float(y2)
     length = ((x2-x1)**2+(y2-y1)**2)**0.5
-    print(f'A ({x1},{y1}); B({x2},{y2}) -> {round(length, 2)}')
+    
+    print(f'A ({x1},{y1}); B({x2},{y2}) -> {float("{0:.3f}".format(length))}')

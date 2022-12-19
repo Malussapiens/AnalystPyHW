130710fc8007f0e82f312b807b7b2399d5d30287
Dead_Moroz <malussapiens@gmail.com>
2022-12-20 00:10:07 +0300

    Some code samples added

--- Seminar2/S2T2/s2t2.py
+++ Seminar2/S2T2/s2t2.py
@@ -5,15 +5,26 @@
 
 print('Программа принимает на вход число N и выдает набор произведений чисел от 1 до N.')
 
-n = int(input('Введите число N:-> '))
+num = int(input('Введите число N:-> '))
 # prods = [1]
 # # print(prods)
 # for i in range(2, n+1):
 #     prods.append(prods[i-2]*i)
 # print(prods)
 
-a=1
-for i in range(2, n+1):
-    print(a, end = ', ')
-    a=a*i
-print(a)
+# a=1
+# for i in range(2, n+1):
+#     print(a, end = ', ')
+#     a=a*i
+# print(a)
+
+# sum = 1
+# for _ in range(num):
+#     sum *= _ + 1
+#     print(sum, end= ' ')
+
+sum = 1
+print(sum, end=' ')
+for n in range(2, num+1):
+    sum *= n
+    print(sum, end=' ')

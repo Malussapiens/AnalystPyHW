345a7c50baff6d2de0ddd0cc6d44cffc88658091
Dead_Moroz <malussapiens@gmail.com>
2023-01-17 01:50:29 +0300

    Task4 updated

--- Seminar5/S5T2/s5t2.py
+++ Seminar5/S5T2/s5t2.py
@@ -57,6 +57,27 @@ def show_score():
     print(f'{player2_name}: {player2_candies} конфет')
 
 
+
+# Выбор кол-ва игроков
+# Варианты: 1 игрок (с ботом), 2 игрока
+game_mode = 0
+while game_mode < 1 or game_mode > 2:
+    system('cls')
+    print('Сколько человек играет?')
+    print('1 - один игрок против компьютера')
+    print('2 - два игрока')
+    game_mode = input('Ваш выбор (1 или 2)?: ')
+    if validate_int(game_mode):
+        game_mode = int(game_mode)
+    else:
+        game_mode = 0
+
+# Знакомимся с игроками
+system('cls')
+print('Давайте знакомиться!')
+player_names = get_player_names(game_mode)
+player1_name, player2_name = player_names
+
 # Инициализируем переменные
 candies = 58
 player1_candies = 0
@@ -64,77 +85,53 @@ player2_candies = 0
 turn = randint(1, 2)
 stake_limit = 28
 
-def main():
-    # Выбор кол-ва игроков
-    # Варианты: 1 игрок (с ботом), 2 игрока
-    game_mode = 0
-    while game_mode < 1 or game_mode > 2:
-        system('cls')
-        print('Сколько человек играет?')
-        print('1 - один игрок против компьютера')
-        print('2 - два игрока')
-        game_mode = input('Ваш выбор (1 или 2)?: ')
-        if validate_int(game_mode):
-            game_mode = int(game_mode)
-        else:
-            game_mode = 0
-    # Знакомимся с игроками
-    system('cls')
-    print('Давайте знакомиться!')
-    player_names = get_player_names(game_mode)
-    player1_name, player2_name = player_names
-
-    
-
-    # Приветствие
-    print(f'{player1_name}, {player2_name}, давайте сыграем в игру.')
-    print(f'На столе лежит {candies} конфета. Играют два игрока, делая ход друг после друга.')
-    print(f'Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем {stake_limit} конфет.')
-    print('Все конфеты оппонента достаются сделавшему последний ход.')
-    print(f'Первым ходит {player_names[turn - 1]}')
-    pause()
+# Приветствие
+print(f'{player1_name}, {player2_name}, давайте сыграем в игру.')
+print(f'На столе лежит {candies} конфета. Играют два игрока, делая ход друг после друга.')
+print(f'Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем {stake_limit} конфет.')
+print('Все конфеты оппонента достаются сделавшему последний ход.')
+print(f'Первым ходит {player_names[turn - 1]}')
+pause()
 
 
-    # основной цикл
-    while candies > 0:
-        system('cls')
-        show_score()
-        if turn == 1:
-            print(f'Ход игрока {player1_name}')
-            stake = player_stake()
-        else:
-            print(f'Ход игрока {player2_name}')
-            if game_mode == 1:
-                system('cls')
-                stake = bot_stake()
-                pause()
-            else:
-                print(stake)
-                stake = player_stake()
-        
-        if stake < 1 or stake > stake_limit or stake > candies:
-            print('Недопустимый ход!')
-            if stake <= candies:
-                print(f'Можно брать от 1 до {stake_limit} конфет')
-                c = input('Нажмите любую клавишу')
-            else:
-                print(f'Можно брать от 1 до {candies} конфет')
-                c = input('Нажмите Enter')
-        else:
-            if turn == 1:
-                player1_candies += stake
-                turn = 2
-            else:
-                player2_candies += stake
-                turn = 1
-            candies -= stake
+# основной цикл
+while candies > 0:
     system('cls')
     show_score()
-
-    # Определяем победителя
-    if turn == 2:
-        print(f'{player1_name} выиграл!')
+    if turn == 1:
+        print(f'Ход игрока {player1_name}')
+        stake = player_stake()
     else:
-        print(f'{player2_name} выиграл!')
-if __name__ == '__main__':
-    main()
+        print(f'Ход игрока {player2_name}')
+        if game_mode == 1:
+            system('cls')
+            stake = bot_stake()
+            pause()
+        else:
+            print(stake)
+            stake = player_stake()
+    
+    if stake < 1 or stake > stake_limit or stake > candies:
+        print('Недопустимый ход!')
+        if stake <= candies:
+            print(f'Можно брать от 1 до {stake_limit} конфет')
+            c = input('Нажмите любую клавишу')
+        else:
+            print(f'Можно брать от 1 до {candies} конфет')
+            c = input('Нажмите Enter')
+    else:
+        if turn == 1:
+            player1_candies += stake
+            turn = 2
+        else:
+            player2_candies += stake
+            turn = 1
+        candies -= stake
+system('cls')
+show_score()
+
+# Определяем победителя
+if turn == 2:
+    print(f'{player1_name} выиграл!')
+else:
+    print(f'{player2_name} выиграл!')

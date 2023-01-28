9c23ba5d2949bc1f97dbdc0a5e59c7bff94ce028
Dead_Moroz <malussapiens@gmail.com>
2023-01-29 00:45:00 +0300

    bug fixed

--- Seminar9/tic_tac_toe/src.py
+++ Seminar9/tic_tac_toe/src.py
@@ -43,6 +43,7 @@ def is_vacant(position):
     
 
 table = [' ' for _ in range(9)]
+print(table)
 wins = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
 stop = False
 player1, player2 = get_player_names(2)
